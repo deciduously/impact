e0464841a9fc959c13a20cfc0f0a6716d603fe5c
deciduously <github@deciduously.com>
2018-05-06 21:55:35 -0400

    Refactor Time

--- src/types/time.rs
+++ src/types/time.rs
@@ -14,23 +14,18 @@ impl Time {
     }
 }
 
+// TODO only show up to the highest amount
+// get the string first, pass the pre-found thing to the write! call, etc
 impl fmt::Display for Time {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let (d, h, m, s) = dhms(self.seconds);
-        write!(f, "{}d{}h{}m{}s", d, h, m, s)
+        let s = self.seconds;
+        write!(
+            f,
+            "{}d{}h{}m{}s",
+            s / (60 * 60 * 24),
+            s / (60 * 60),
+            s / 60,
+            u64::rem(s, 60)
+        )
     }
-}
-
-fn mins(s: u64) -> (u64, u64) {
-    (s / 60, u64::rem(s, 60))
-}
-
-fn hms(s: u64) -> (u64, u64, u64) {
-    let (m, s) = mins(s);
-    (m / 60, m, s)
-}
-
-fn dhms(s: u64) -> (u64, u64, u64, u64) {
-    let (h, m, s) = hms(s);
-    (h / 24, h, m, s)
 }
