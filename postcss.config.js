d8f8a16f606f1adac3a53a135953fdf7f1d79728
Ben Lovy <ben@deciduously.com>
2020-02-21 21:50:02 -0500

    Partial update - borken style

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,7 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 module.exports = {
   syntax: 'postcss-scss',
   plugins: [
     require("autoprefixer"),
-    purgecss({
-      content: ['./**/*.html']
-    }),
     require("cssnano")({
       preset: "default"
     })
